ec1ca5a7186e815af8235e21a8ec61255674b108
Sravanth C <54331348+chebro@users.noreply.github.com>
2021-05-16 18:43:58 +0530

    add mount points for route middleware

--- server/index.js
+++ server/index.js
@@ -1,30 +1,35 @@
 // Import dependencies
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
-const { execCmd } = require('./command');
+const ttydRouter = require('./routes/ttydRouter');
 
 // Create a new express application named 'app'
 const app = express();
 
 // Set our backend port to be either an environment variable or port 5000
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
+
+// Parsing json/application
+app.use(cors());
+app.use(express.json());
 
 // This application level middleware prints incoming requests to the servers console, useful to see incoming requests
 app.use((req, res, next) => {
-    console.log(`Request_Endpoint: ${req.method} ${req.url}`);
-    next();
+  console.log(`Request_Endpoint: ${req.method} ${req.url}`);
+  next();
 });
 
 // server-side rendering ~ no?
 //app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/:cmd', execCmd);
+app.use('/ttyd', ttydRouter);
 
 // Catch any bad requests
 app.get('*', (req, res) => {
-    res.status(200).json({
-        msg: 'Catch All'
-    });
+  res.status(200).json({
+    msg: 'Catch All',
+  });
 });
 
 // Configure our server to listen on the port defiend by our port variable
